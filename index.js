ba0c26a2025e0953f06dfc54da6bf2fe9055bd64
Ben Allport <you@example.com>
2026-09-17 21:22:39 +0100

    add the message content intent

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Client, Events, GatewayIntentBits, Partials } from 'discord.js';
 import registerEventHandlers from './events/eventHandler.js';
 import config from '#config' with { type: "json" };
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions], partials: [Partials.Message, Partials.Channel, Partials.Reaction] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent], partials: [Partials.Message, Partials.Channel, Partials.Reaction] });
 
 registerEventHandlers(client);
 
